d0429624782055a34de701880e172506871030c1
abderrahimelamarty <abdrahim.tawnat12345@gmail.com>
2023-02-13 11:54:22 +0100

    first_version

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Col, Container, Row } from "reactstrap";
-import Subtitle from "../common/Subtitle.Js";
 
 import UserService from "../services/user.service";
 import Offers from "./Offers/Offers";
-
+import "./Home.css";
 const Home = () => {
   const [content, setContent] = useState("");
 
@@ -31,9 +30,40 @@ const Home = () => {
           <Col lg="6">
             <div className="hero__content">
               <div className="hero__subtitle d-flex align-items-center">
-                <Subtitle Subtitle={"know before you go"} />
-                <img src="" />
+                <img src="https://liv.rent/blog/wp-content/uploads/2020/01/2020-02-03-RoommateLiving2-01-min.png" />
               </div>
+              <h1>
+                Find your Favorite Rommates and create{" "}
+                <span className="highlight"> memories</span>
+              </h1>
+              <p>
+                Implement the recommendation logic in your application code. For
+                this example, let's say we have a Spring Boot app that sells
+              </p>
+            </div>
+          </Col>
+          <Col lg="2">
+            <div className="hero__img-box">
+              <img
+                src="https://media.houseandgarden.co.uk/photos/637637c2e4eb0449205a261c/1:1/w_1500,h_1500,c_limit/Shot-04_068_RT.jpg"
+                alt=""
+              />
+            </div>
+          </Col>
+          <Col lg="2">
+            <div className="hero__img-box mt-4">
+              <img
+                src="https://rnb.scene7.com/is/image/roomandboard/HH_Liv_S1021_1022?wid=1200"
+                alt=""
+              />
+            </div>
+          </Col>
+          <Col lg="2">
+            <div className="hero__img-box mt-5">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Imperial_Hotel_Osaka_regular_floor_standard_twin_room_20120630-001.jpg/1200px-Imperial_Hotel_Osaka_regular_floor_standard_twin_room_20120630-001.jpg"
+                alt=""
+              />
             </div>
           </Col>
         </Row>
